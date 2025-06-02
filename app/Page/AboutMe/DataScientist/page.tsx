7681ae2ec80b49d0b169aa2dceb44465f27c3252
Benjamin <benjaminenma99@gmail.com>
2025-06-02 12:44:52 -0400

    udate DataScientist and others

--- app/Page/AboutMe/DataScientist/page.tsx
+++ app/Page/AboutMe/DataScientist/page.tsx
@@ -1,32 +1,109 @@
 import { CardCourse } from "@/components/CardCourse/page";
-import GetData from "@/lib/Api/ProfessionalDevelopment/ProfessionalDevelopment"
+import { CardCode } from "@/components/CardCode/page";
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import {GetData, GetDataProjects} from "@/lib/Api/ProfessionalDevelopment/ProfessionalDevelopment"
 
 export default async function DataScientist(){
 
     const data = GetData();
+    const projects = GetDataProjects();
         
     return(
 
-        <div className="grid place-content-center mt-10 max-sm:mt-5">
+        <div className="grid place-content-center mt-10 max-sm:mt-5 max-sm:w-100">
             
-            <div className="text-center bg-blue-500/15 m-10 p-4 mx-auto rounded-full shadow-lg shadow-blue-500/30 max-sm:p-0.5 max-w-475">
-                <h1 className="text-7xl font-bold text-center">Data Scientist</h1>
+            <div className="text-center bg-blue-500/15 m-10 p-4 mx-auto rounded-full shadow-lg shadow-blue-500/30 max-sm:p-0.5 w-375 max-sm:w-100">
+                <h1 className="text-7xl font-bold text-center max-sm:text-xl">Data Scientist</h1>
             
-                <p className="text-4xl m-4">
+                <p className="text-4xl m-4 max-sm:text-xs">
                     Durante 2024, completé diversas formaciones en Ciencia de Datos, disciplina que constituye mi principal interés profesional. Como siguiente objetivo, planeo certificarme en <a href="https://www.coursera.org/specializations/machine-learning-introduction#courses" className="text-blue-500"> Machine learning</a>. Cursos relevantes realizados:
                 </p>
             </div>
 
-            <ul className="grid grid-cols-4 max-sm:grid-cols-1">
-                {
-                    data.map( (dt,index) => (
-                        <li key={index} className="grid place-content-center">
-                            <CardCourse href={dt.url} name={dt.nombre} date={dt.fecha}/>
-                        </li>
-                    ) )
-                }
-            </ul>
+            <div className="grid place-content-center max-sm:mb-10">
 
+                <Carousel className="w-350 max-sm:w-75 max-sm:h-40">
+                
+                    <CarouselContent className="-ml-1">
+                
+                        {data.map(( df, index) => (
+                
+                            <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
+                            
+                                <div className="p-1">
+                            
+                                    <Card>
+                            
+                                        <CardContent className="flex aspect-square items-center justify-center p-6 max-sm:h-45 max-sm:w-75">
+                            
+                                            <CardCourse href={df.url} name={df.nombre} date={df.fecha}/>
+                            
+                                        </CardContent>
+                            
+                                    </Card>
+                            
+                                </div>
+                            
+                            </CarouselItem>
+                        
+                        ))}
+                    
+                    </CarouselContent>
+                    
+                    <CarouselPrevious />
+                    
+                    <CarouselNext />
+                
+                </Carousel>
+
+            </div>
+
+            <div className="grid place-content-center mt-15">
+
+                <h1 className="text-6xl italic font-semibold mb-5 text-center max-sm:text-xl">Proyectos realizados</h1>
+
+                <Carousel className="w-350 max-sm:w-75 max-sm:h-40">
+                
+                    <CarouselContent className="-ml-1">
+                
+                        {projects.map(( df, index) => (
+                
+                            <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
+                            
+                                <div className="p-1">
+                            
+                                    <Card>
+                            
+                                        <CardContent className="flex aspect-square items-center justify-center p-6 max-sm:h-45 max-sm:w-75">
+                            
+                                            <CardCode href={df.href} name={df.name} programmingLanguage={df.programmingLanguage} description={df.description} />
+                                            
+                                        </CardContent>
+                            
+                                    </Card>
+                            
+                                </div>
+                            
+                            </CarouselItem>
+                        
+                        ))}
+                    
+                    </CarouselContent>
+                    
+                    <CarouselPrevious />
+                    
+                    <CarouselNext />
+                
+                </Carousel>
+
+            </div>
             
             
         </div>
--- components/CardCourse/page.tsx
+++ components/CardCourse/page.tsx
@@ -14,9 +14,9 @@ interface Props {
 
 export function CardCourse({ href, name, date }:Props) {
   return (
-    <Card className="w-full max-w-sm shadow-lg shadow-gray-500/30 max-sm:m-2 mb-5 bg-violet-400/40 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-violet-400/70">
+    <Card className="w-full h-75 max-sm:h-50 max-sm:w-50 max-w-sm shadow-lg shadow-gray-500/30 max-sm:m-2 mb-5 bg-violet-400/40 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-violet-400/70">
       <CardHeader>
-        <CardTitle className="text-3xl font-normal max-sm:text-xs">{ name }</CardTitle>
+        <CardTitle className="text-3xl font-normal max-sm:text-xs italic">{ name }</CardTitle>
       </CardHeader>
       <CardContent>
         <p className="text-xl font-normal max-sm:text-xs"> Se realizó: { date }</p>
